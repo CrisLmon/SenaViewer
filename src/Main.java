b178151168ae4bc45873eba11a7343a9708d1558
Cristian <179995201+CrisLmon@users.noreply.github.com>
2025-06-04 20:33:49 -0500

    featMenu: Creación de mensaje inicial

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+ public static void main(String[] args) {
+
+
+    System.out.println("\nMenú principal\n\n");
+    System.out.println("1. Movies\n2. Series\n3. Books\n4. Magazines\n5. Exit\n");
+    System.out.println("\nSeleccione una de las siguientes opciones:\n");
+
+ }
 }
