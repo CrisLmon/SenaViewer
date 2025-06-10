c03af119b64d98f9cdef25702514903052403f38
wilder898 <wilder898@gmail.com>
2025-06-09 23:19:33 -0500

    Creacion de Array y metodos mostrarNombre

--- src/edu/misena/senaviewer/model/Book.java
+++ src/edu/misena/senaviewer/model/Book.java
@@ -1,11 +1,27 @@
 package edu.misena.senaviewer.model;
 
+import java.util.ArrayList;
+
 public class Book extends Publication { 
     private String authors;
+    static ArrayList<String> nombres = new ArrayList<>();
+    
+    static{
+        nombres.add("Cin años de soledad.");
+        nombres.add("Principito.");
+        nombres.add("El amor en los tiempos del cólera.");
+        nombres.add("Los juegos del hambre.");
+    }
 
     public Book(String title, String editionDate, String editorial, String authors) {
         super(title, editionDate, editorial);
         this.authors = authors;
+    }
+
+    public static void mostrarNombres(){
+        for (int count = 0; count < nombres.size(); count++){
+            System.out.println((count + 1) + ") " + nombres.get(count));
+        }
     }
 
     public String getAuthors() {
--- src/edu/misena/senaviewer/model/Magazine.java
+++ src/edu/misena/senaviewer/model/Magazine.java
@@ -1,11 +1,27 @@
 package edu.misena.senaviewer.model;
 
+import java.util.ArrayList;
+
 public class Magazine extends Publication {
     private String authors;
+    static ArrayList<String> revista = new ArrayList<>();
+
+    static{
+        revista.add("Soho.");
+        revista.add("Diners.");
+        revista.add("Time.");
+        revista.add("Vogue.");
+    }
 
     public Magazine(String title, String editionDate, String editorial, String authors) {
         super(title, editionDate, editorial);
         this.authors = authors;
+    }
+
+    public static void mostrarNombres(){
+        for (int count = 0; count < revista.size(); count++){
+            System.out.println((count + 1) + ") " + revista.get(count));
+        }
     }
 
     public String getAuthors() {
